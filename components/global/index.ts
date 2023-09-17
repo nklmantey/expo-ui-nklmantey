994273c56927bdc0b8d926a0d4643d2fc174bdaf
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-09-17 15:48:26 +0000

    setting up global components

---
+++ components/global/index.ts
@@ -0,0 +1,17 @@
+import {
+  HeadingText,
+  BoldText,
+  ExtraBoldText,
+  RegularText,
+  SubHeadingText,
+} from "./StyledText";
+import Card from "./Card";
+
+export {
+  HeadingText,
+  SubHeadingText,
+  BoldText,
+  ExtraBoldText,
+  RegularText,
+  Card,
+};
