5a310649d7a759dd899d28f064baec60f1e1cd07
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-09-17 15:45:57 +0000

    project init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import useCachedResources from "./hooks/useCachedResources";
+import RootNavigation from "./navigation/RootNavigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <StatusBar style="dark" />
+      <RootNavigation />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
