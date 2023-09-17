4d555e130953d248050b41d156b86aecde138f97
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-09-17 15:47:18 +0000

    navigation set up

---
+++ navigation/RootNavigation.tsx
@@ -0,0 +1,34 @@
+import {
+  TransitionPresets,
+  createStackNavigator,
+} from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import ButtonScreen from "../screens/ButtonScreen";
+import InputScreen from "../screens/InputScreen";
+import AlertScreen from "../screens/AlertScreen";
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const RootNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.SlideFromRightIOS,
+          animationEnabled: true,
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+        }}
+      >
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="ButtonScreen" component={ButtonScreen} />
+        <Stack.Screen name="InputScreen" component={InputScreen} />
+        <Stack.Screen name="AlertScreen" component={AlertScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
